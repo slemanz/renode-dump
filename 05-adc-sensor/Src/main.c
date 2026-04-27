0376510a592a2e7c1541a9d999297fb48c138276
slemanz <william_sleman@hotmail.com>
2026-04-27 18:49:36 -0300

    adc sensor(feat): add uart

--- 05-adc-sensor/Src/main.c
+++ 05-adc-sensor/Src/main.c
@@ -30,6 +30,44 @@ typedef enum {
 static sensor_state_t g_sensor_state = SENSOR_OK;
 
 /* -------------------------------------------------------------------------- */
+/*  UART2                                                                      */
+/* -------------------------------------------------------------------------- */
+
+static void uart2_init(void)
+{
+    GPIO_PinConfig_t tx = {
+        .pGPIOx = GPIOA, .GPIO_PinNumber = GPIO_PIN_NO_2,
+        .GPIO_PinMode = GPIO_MODE_ALTFN, .GPIO_PinSpeed = GPIO_SPEED_FAST,
+        .GPIO_PinOPType = GPIO_OP_TYPE_PP, .GPIO_PinPuPdControl = GPIO_PIN_PU,
+        .GPIO_PinAltFunMode = PA2_ALTFN_UART2_TX
+    };
+    GPIO_Init(&tx);
+
+    GPIO_PinConfig_t rx = {
+        .pGPIOx = GPIOA, .GPIO_PinNumber = GPIO_PIN_NO_3,
+        .GPIO_PinMode = GPIO_MODE_ALTFN, .GPIO_PinSpeed = GPIO_SPEED_FAST,
+        .GPIO_PinOPType = GPIO_OP_TYPE_PP, .GPIO_PinPuPdControl = GPIO_PIN_PU,
+        .GPIO_PinAltFunMode = PA3_ALTFN_UART2_RX
+    };
+    GPIO_Init(&rx);
+
+    UART_Config_t cfg = {
+        .pUARTx = UART2, .UART_Baud = UART_STD_BAUD_115200,
+        .UART_Mode = UART_MODE_TXRX, .UART_WordLength = UART_WORDLEN_8BITS,
+        .UART_NoOfStopBits = UART_STOPBITS_1, .UART_ParityControl = UART_PARITY_DISABLE,
+        .UART_HWFlowControl = UART_HW_FLOW_CTRL_NONE
+    };
+    UART_Init(&cfg);
+    UART_PeripheralControl(UART2, ENABLE);
+}
+
+int __io_putchar(int ch)
+{
+    UART_WriteByte(UART2, (uint8_t)ch);
+    return ch;
+}
+
+/* -------------------------------------------------------------------------- */
 /*  Main                                                                       */
 /* -------------------------------------------------------------------------- */
 
